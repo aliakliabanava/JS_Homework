ac66886957b95a02746cf07612496bb2dc83f0d1
aliakliabanava <Alia.Kliabanava@gmail.com>
2022-01-25 21:24:58 +0100

    corrections

--- Homework_8/HW8.js
+++ Homework_8/HW8.js
@@ -1,10 +1,8 @@
 //Task 1
 function filterNumbersArr(numbers) {
-    var newArr = numbers.filter(function(el) {
+    return numbers.filter(function(el) {
         return el > 0;
     });
-
-    return newArr;
 }
 
 filterNumbersArr([-1, 0, 2, 34, -2]);
@@ -12,25 +10,16 @@ filterNumbersArr([-1, 0, 2, 34, -2]);
 
 //Task 2
 function findPositiv(arr) {
-    var found = arr.find(function (element) {
+    return arr.find(function (element) {
         return element > 0;
     });
-
-    return (found);
 }
 
-    findPositiv([-1, 0, 2, 34, -2]);
+findPositiv([-1, 0, 2, 34, -2]);
 
 //Task 3
 function isPalindrome(str) {
-    var strNew = str.toLowerCase().split('').reverse().join('');
-    var strOld = str.toLowerCase();
-
-    if (strNew == strOld) {
-        return true;
-    } else {
-        return false;
-    }
+    return str.toLowerCase().split('').reverse().join('') == str.toLowerCase();
 }
 
 isPalindrome('шалаШ'); // true
@@ -39,14 +28,7 @@ isPalindrome('привет'); // false
 
 //Task 4
 function areAnagrams(str1, str2) {
-    var str1New = str1.toLowerCase().split('').sort().join('');
-    var str2New = str2.toLowerCase().split('').sort().join('');
-
-    if (str1New == str2New) {
-        return true;
-    } else {
-        return false;
-    }
+    return str1.toLowerCase().split('').sort().join('') == str2.toLowerCase().split('').sort().join('');
 }
 
 areAnagrams('кот', 'отк'); // true
